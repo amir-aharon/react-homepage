5260c22160f569a4d528719074044348e19108de
Amir Aharon <amir.the.junior@gmail.com>
2022-06-24 16:48:52 +0300

    Updates

---
+++ precache-manifest.bffe6e1ed9b1c2d3c3768eaefe83aa4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5546c478ef9f87c46bee",
+    "url": "/react-homepage/static/css/main.85197642.chunk.css"
+  },
+  {
+    "revision": "5546c478ef9f87c46bee",
+    "url": "/react-homepage/static/js/main.5546c478.chunk.js"
+  },
+  {
+    "revision": "80ea1c632dafcfef2509",
+    "url": "/react-homepage/static/js/1.80ea1c63.chunk.js"
+  },
+  {
+    "revision": "4160331b5d924eb77e41",
+    "url": "/react-homepage/static/js/runtime~main.4160331b.js"
+  },
+  {
+    "revision": "82b9475fae87193240fbcb0c52fef9d9",
+    "url": "/react-homepage/index.html"
+  }
+];
